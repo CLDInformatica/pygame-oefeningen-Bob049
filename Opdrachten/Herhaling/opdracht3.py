ba21133147437a58d43d00520ec1ee24c5788923
Bob049 <123565@chrlyceumdelft.nl>
2025-09-08 13:17:33 +0000

    les1

--- Opdrachten/Herhaling/opdracht3.py
+++ Opdrachten/Herhaling/opdracht3.py
@@ -1,7 +1,7 @@
 '''
 Gegeven is een lijst met games. Doe het volgende:
 
-  - Voeg Super Mario Bros toe aan de lijst
+  - Voeg Super  BroMarios toe aan de lijst
   - Vraag de gebruiker om een **getal** tussen de 0 en de 5
   - Print de game die op deze plek in de lijst staat
   - Vraag de gebruiker om een game met een input
@@ -10,3 +10,9 @@ Gegeven is een lijst met games. Doe het volgende:
 '''
 
 games = ["Minecraft", "Rust", "GTA V", "Hayday", "Clash of clans"]
+
+games.append ("Super Mario Bros")
+
+getal = int(input("kies een getal tussen 0 en de 5"))
+
+print (games.[getal])
